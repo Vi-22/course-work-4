24cd7f273f37aec8115465d73be10420bf0619c4
v.sukhenko <v.sukhenko@just-ai.com>
2022-12-27 17:34:32 +0300

    sql comm corr

--- src/main/resources/requests.sql
+++ src/main/resources/requests.sql
@@ -1,38 +1,38 @@
-/* создание таблицы Альпинист */
+-- создание таблицы Альпинист
 
 CREATE TABLE IF NOT EXISTS tb_climbers(climber_id SERIAL PRIMARY KEY, name VARCHAR(100) NOT NULL,
 address VARCHAR(255) NOT NULL, group_number INTEGER)
 
-/* Добавление данных в таблицу Альпинист*/
+-- Добавление данных в таблицу Альпинист
 
- INSERT INTO tb_climbers (name, address) VALUES (?, ?) RETURNING climber_id */
+ INSERT INTO tb_climbers (name, address) VALUES (?, ?) RETURNING climber_id
 
-/* Изменение имени альпиниста */
+-- Изменение имени альпиниста
 
  UPDATE tb_climbers SET name = ? WHERE climber_id = ?
 
-/* Получение идентификатором и имен альпинистов старше 30 и младше 50 лет */
+-- Получение идентификатором и имен альпинистов старше 30 и младше 50 лет
 
 SELECT climber_id, name FROM tb_climbers WHERE age > 30 AND age < 50
 
-/* Получение названий гор, высота которых больше указанной */
+-- Получение названий гор, высота которых больше указанной
 
 SELECT name FROM tb_mountains WHERE height > ?
 
-/* Получение страны, в которой расположена гора с определенным названием */
+-- Получение страны, в которой расположена гора с определенным названием
 
 SELECT country FROM tb_mountains WHERE name = ?
 
-/* Получение идентификаторов, которые совершали восхождения в прошлом */
+-- Получение идентификаторов, которые совершали восхождения в прошлом
 
 SELECT climber_id FROM tb_group WHERE climbDate < CURRENT_TIMESTAMP
 
-/* Получение идентификаторов групп, которые совершали восхождения на гору с определенным названием */
+-- Получение идентификаторов групп, которые совершали восхождения на гору с определенным названием
 
 SELECT group_number FROM tb_mountains WHERE name = ?
 
-/* Получение идентификатором и имен альпинистов, которые совершали
-восхождения на горы, высота которых от ... до ... */
+-- Получение идентификатором и имен альпинистов, которые совершали
+-- восхождения на горы, высота которых от ... до ...
 
 SELECT climber_id, name FROM tb_climbers WHERE group_number =
 (SELECT group_number FROM tb_mountains WHERE height <= ? AND height >= ?)
